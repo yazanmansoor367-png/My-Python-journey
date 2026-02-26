656e91ea032065c37f16af1f92812e8b8d6a723a
yazan al mansour <yazan.mansoor367@gmail.com>
2026-02-26 18:00:14 +0300

    Create Name_Formatter.py

---
+++ Basics/Name_Formatter.py
@@ -0,0 +1,10 @@
+Fname = input("enter your first name ?")
+Dname = input("enter your Dname name ?")
+Lname = input("enter your Lname name ?")
+
+Fname = Fname.strip().capitalize()
+Dname = Dname.strip().capitalize()
+Lname = Lname.strip().capitalize()
+
+
+print(f"hello {Fname} {Dname :.1s} {Lname}")
